cda27254f88ddc6e771a65158845e64d52b080a4
ohmicode <ohmicode@somewhere.io>
2018-03-31 09:07:24 +0300

    testWithdrawParallel added

--- src/test/java/ohmicode/example/AccountsControllerTest.java
+++ src/test/java/ohmicode/example/AccountsControllerTest.java
@@ -10,6 +10,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -123,4 +130,48 @@ public class AccountsControllerTest {
                 .andExpect(content().string(equalTo("success")));
     }
 
+    @Test
+    public void testWithdrawParallel() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/deposit/1/100").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("success")));
+
+        requestWithdrawParallel(100);
+
+        mvc.perform(MockMvcRequestBuilders.get("/balance/1").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("30.00")));
+
+        //clean
+        mvc.perform(MockMvcRequestBuilders.post("/withdraw/1/30.00").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("success")));
+    }
+
+    private void requestWithdrawParallel(int size) {
+        CountDownLatch countDownLatch = new CountDownLatch(size);
+
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+        IntStream.range(0, size).forEach(i -> {
+            executor.execute(withdrawRunnable(countDownLatch));
+            countDownLatch.countDown();
+        });
+        executor.shutdown();
+        try {
+            executor.awaitTermination(20, TimeUnit.SECONDS);
+        } catch (InterruptedException ie) {
+            executor.shutdownNow();
+        }
+    }
+
+    private Runnable withdrawRunnable(CountDownLatch barrier) {
+        return () -> {
+            try {
+                barrier.await();
+
+                mvc.perform(MockMvcRequestBuilders.post("/withdraw/1/70.00").accept(MediaType.APPLICATION_JSON))
+                        .andExpect(content().string(anything()));
+            } catch (Exception ignored) {}
+        };
+    }
 }
